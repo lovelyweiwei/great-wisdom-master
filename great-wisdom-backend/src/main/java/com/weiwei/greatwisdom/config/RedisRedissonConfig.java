3c1ca6c172905e0bb6c800f806f51551378bbc4d
lovelyweiwei <342048733@qq.com>
2023-07-13 22:28:32 +0800

    修改小优化

--- great-wisdom-backend/src/main/java/com/weiwei/greatwisdom/config/RedisRedissonConfig.java
+++ great-wisdom-backend/src/main/java/com/weiwei/greatwisdom/config/RedisRedissonConfig.java
@@ -1,6 +1,9 @@
 package com.weiwei.greatwisdom.config;
 
 import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +12,29 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+/**
+ * @Author weiwei
+ * @Date 2023/7/5 23:10
+ * @Version 1.0
+ */
+@Data
+@ConfigurationProperties(prefix = "spring.redis")
 @Configuration
-public class RedisConfig {
+public class RedisRedissonConfig {
+    private Integer database;
+    private String host;
+    private Integer port;
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         // 根据你的Redis配置，创建合适的RedisConnectionFactory实例，
         // 例如 JedisConnectionFactory 或 LettuceConnectionFactory
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName("localhost");
-        config.setPort(6379);
-        config.setPassword("666");
-        config.setDatabase(1);
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+        config.setDatabase(database);
 
         return new LettuceConnectionFactory(config);
     }
@@ -33,4 +47,13 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+                .setDatabase(database)
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password);
+        return Redisson.create(config);
+    }
 }
